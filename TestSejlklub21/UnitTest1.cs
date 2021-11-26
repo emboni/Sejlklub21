50b360dafba2d90d2e18a2a7d194f36848b8a212
Emil Bodholdt Nielsen <idvx3230@edu.zealand.dk>
2021-11-26 13:01:25 +0100

    Add project files.

---
+++ TestSejlklub21/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSejlklub21
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Assert
+            Assert.Fail();
+        }
+    }
+}
