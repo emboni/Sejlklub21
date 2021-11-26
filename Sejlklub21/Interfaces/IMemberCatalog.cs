0a8bcb454642751018267d1ece039b1466489728
catsmeow4753 <77382504+catsmeow4753@users.noreply.github.com>
2021-11-26 13:37:45 +0100

    Update

---
+++ Sejlklub21/Interfaces/IMemberCatalog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sejlklub21.Models;
+
+namespace Sejlklub21.Interfaces
+{
+    public interface IMemberCatalog
+    {
+        public void AddMember(Member member);
+        public void UpdateMember(Member member);
+        public void DeleteMember(Member member);
+
+        public Member GetMember();
+        public List<Member> GetAllMembers();
+    }
+}
---
+++ Sejlklub21/Services/MemberCatalog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sejlklub21.Interfaces;
+using Sejlklub21.Models;
+
+namespace Sejlklub21.Services
+{
+    public class MemberCatalog : IMemberCatalog
+    {
+        public void AddMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetMember()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> GetAllMembers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
