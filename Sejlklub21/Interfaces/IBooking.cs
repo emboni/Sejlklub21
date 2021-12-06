acd62138238e5ab50be3490357fce4ac921e2a5c
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2021-12-06 10:38:39 +0100

    IBooking and IBookingCatalog

---
+++ Sejlklub21/Interfaces/IBooking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sejlklub21.Models;
+
+namespace Sejlklub21.Interfaces
+{
+    public interface IBooking
+    {
+        int Id { get; set; }
+        IBoat Boat { get; set; }
+        IMember Member { get; set; }
+        Journey Journey { get; set; }
+    }
+}
--- Sejlklub21/Interfaces/IBookingCatalog.cs
+++ Sejlklub21/Interfaces/IBookingCatalog.cs
@@ -7,5 +7,14 @@ namespace Sejlklub21.Interfaces
 {
     interface IBookingCatalog
     {
+        void Add(IBooking booking);
+
+        void Update(IBooking booking);
+
+        void Delete(int bookingId);
+
+        IBooking GetBooking(int bookingId);
+
+        List<IBooking> GetAllBookings();
     }
 }
